3d93ba3e6a174598651e384d1ae8d4197ddec32e
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-02-06 18:49:13 -0500

    adding stateless components to each subcomponents

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+           <footer className= "footer">
+           <div className= "bottom">
+            Click Game! 
+           </div>
+           </footer> 
+        </div>
+    )
+}
