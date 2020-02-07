746e987672f4de0947731efa2fd91723613241d7
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-02-06 19:16:07 -0500

    flags assest completed

---
+++ src/data/index.js
@@ -0,0 +1,35 @@
+import brazilian from "../assets/brazilianflag.jpg";
+import denmark from "../assets/denmarkflag.jpg";
+import indian from "../assets/indianflag.jpg";
+import italian from "../assets/italianflag.jpg";
+import korean from "../assets/korean.jpg";
+import usa from "../assets/usaflag.jpg";
+
+const FLAGS = [
+    {
+        id: 1,
+        image : brazilian
+    },
+    {
+        id: 2,
+        image : denmark
+    },
+    {
+        id: 3,
+        image : indian
+    },
+    {
+        id: 4,
+        image : italian
+    },
+    {
+        id: 5,
+        image : korean
+    },
+    {
+        id: 6,
+        image : usa
+    }
+]
+
+export default FLAGS
